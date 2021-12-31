7432c477e094df67de7ed907487e6b4774cc2fdd
Hussaini Muhammad Auwal <hauwal4969@gmail.com>
2021-12-31 13:31:36 +0100

    crpto-js nodule added

--- scripts/Block.js
+++ scripts/Block.js
@@ -1,9 +1,21 @@
+const SHA256 = require("crypto-js/sha256");
+
 class Block{
-	constructor(){
-		
+
+	constructor(timestamp, transactions, previousHash = '' ){
+		this.timestamp = timestamp;
+		this.transactions = transactions;
+		this.previousHash = previousHash;
+		this.nonce = 0;
+		this.hash = this.calculateHash();
+	}
+
+	calculateHash() {
+		return SHA256(this.previousHash + this.timestamp + JSON.stringify(this.transactions) + this.nonce).toString();
 	}
 }
 
-let instance = new Block();
+let instance = new Block(0,60);
+console.log(instance.hash);
 
 module.exports = Block;
