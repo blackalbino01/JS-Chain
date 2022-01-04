32f9b447af4d200dcfe82aae5dd845301137888e
Hussaini Muhammad Auwal <hauwal4969@gmail.com>
2022-01-04 06:28:24 +0100

    retriving previous blo block function added

--- scripts/Blockchain.js
+++ scripts/Blockchain.js
@@ -15,9 +15,14 @@ class Blockchain{
 		const block = new Block(Date.parse('2022-01-03'), [], 0, 0);
 		return block;
 	}
+
+	//Retrieving the Previous block
+	getPreviousBlock(){
+		return this.chain[this.chain.length - 1];
+	}
 }
 
 let instance = new Blockchain();
-console.log(instance.createGenesisBlock());
+console.log(instance.getLatestBlock());
 
 module.exports = Blockchain;
