6494493a2d16abeb47d77bd4e16ffdf3026555cb
Hussaini Muhammad Auwal <hauwal4969@gmail.com>
2022-01-04 13:21:57 +0100

    Signing Transaction function added

--- scripts/Transaction.js
+++ scripts/Transaction.js
@@ -1,3 +1,8 @@
+const { sha256 } = require("ethereum-cryptography/sha256");
+const { utf8ToBytes,toHex } = require("ethereum-cryptography/utils");
+const secp256k1 = require("secp256k1");
+const { getRandomBytesSync } = require("ethereum-cryptography/random");
+
 class Transaction{
 	constructor(fromAddress, toAddress, amount){
 		this.fromAddress = fromAddress;
@@ -8,16 +13,37 @@ class Transaction{
 
 	//Calculating Hash String using SHA256
 	calculateHash() {
-		return toHex(sha256(utf8ToBytes(this.previousHash + this.timestamp + JSON.stringify(this.transactions) + this.nonce)));
+		return sha256(utf8ToBytes(this.fromAddress + this.toAddress + this.amount.toString()));
 	}
 
 	//Signing Transactions using the signature.
-	signature(){
+	signTx(privKey){
+
+		// get the public key in a compressed format
+		const pubKey = toHex(secp256k1.publicKeyCreate(privKey));
+		console.log('PUBLIC KEY : ',pubKey);
+
+		/*if(pubKey !== this.fromAddress) {
+
+	        throw new Error('You cannot sign transactions for other wallets!');
+	    }*/
+
+	    const TxId = this.calculateHash();
+
+	    // sign the message
+		const sig = secp256k1.ecdsaSign(TxId, privKey);
+
+		this.signature = sig;
+        console.log('SIGNATURE : ',toHex(sig.signature));
 
 	}
 
 }
 
-let instance = new Transaction();
+let instance = new Transaction(0,0,1);
 
-module.exports = Transaction;
+const hash = instance.calculateHash();
+
+instance.signTx(hash);
+
+module.exports.Transaction = Transaction;
